b86b19e7c67580d4df2caecd85c3f479c3321598
adeard <adeardian1994@gmail.com>
2024-01-24 21:48:43 +0700

    update register layout

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Row,Col,Container,Form} from 'react-bootstrap';
+import {Row,Col,Container,Form, Button} from 'react-bootstrap';
 
-class Register extends React.Component {
+const Register = () => {
 
-	render() {
+	const onSubmit = (e) => {
+		e.preventDefault()
+		console.log(e.target.inputEmail.value)
+		console.log(e.target.inputPassword.value)
+	}
+
+
     	return (
     	  <Container fluid className='bg-white'>
 	         <Row>
@@ -15,7 +21,7 @@ class Register extends React.Component {
 	                     <Row>
 	                        <Col md={9} lg={8} className="mx-auto pl-5 pr-5">
 	                           <h3 className="login-heading mb-4">Daftar</h3>
-	                           <Form>
+	                           <Form onSubmit={onSubmit}>
 	                              <div className="form-label-group">
 	                                 <Form.Control type="email" id="inputEmail" placeholder="Email address" />
 	                                 <Form.Label htmlFor="inputEmail">Email / Mobile</Form.Label>
@@ -24,11 +30,20 @@ class Register extends React.Component {
 	                                 <Form.Control type="password" id="inputPassword" placeholder="Password" />
 	                                 <Form.Label htmlFor="inputPassword">Password</Form.Label>
 	                              </div>
-	                              <div className="form-label-group mb-4">
-	                                 <Form.Control type="text" id="inputPromo" placeholder="Promocode" />
-	                                 <Form.Label htmlFor="inputPromo">Promocode</Form.Label>
+								  <div className="form-label-group">
+	                                 <Form.Control type="password" id="inputConfirmPassword" placeholder="Confirm Password" />
+	                                 <Form.Label htmlFor="inputConfirmPassword">Confirm Password</Form.Label>
 	                              </div>
-	                              <Link to="/login" className="btn btn-lg btn-outline-primary btn-block btn-login text-uppercase font-weight-bold mb-2">Daftar</Link>
+								  <div className="form-label-group">
+	                                 <Form.Control type="text" id="inputFullname" placeholder="Fullname" />
+	                                 <Form.Label htmlFor="inputFullname">Fullname</Form.Label>
+	                              </div>
+	                              <div className="form-label-group">
+	                                 <Form.Control type="text" id="inputPhoneNumber" placeholder="Phone Number" />
+	                                 <Form.Label htmlFor="inputPhoneNumber">Phone Number</Form.Label>
+	                              </div>
+								  <Button variant="link" type="submit" className="btn btn-lg btn-outline-primary btn-block btn-login text-uppercase font-weight-bold mb-2"> Daftar </Button>
+	                              
 	                              <div className="text-center pt-3">
 	                                 Sudah mempunyai akun? <Link className="font-weight-bold" to="/login">Masuk</Link>
 	                              </div>
@@ -41,7 +56,6 @@ class Register extends React.Component {
 	         </Row>
 	      </Container>
     	);
-    }
 }
 
 
